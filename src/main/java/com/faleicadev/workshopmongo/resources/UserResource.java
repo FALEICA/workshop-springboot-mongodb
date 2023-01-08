033b39dd7ab56fd7210c3b745f444fc7d9a606da
FALEICA <fabio.leite.cardoso@gmail.com>
2023-01-07 23:41:48 -0300

    Lançando excecoes personalizadas

--- src/main/java/com/faleicadev/workshopmongo/resources/UserResource.java
+++ src/main/java/com/faleicadev/workshopmongo/resources/UserResource.java
@@ -1,14 +1,18 @@
 package com.faleicadev.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.faleicadev.workshopmongo.domain.User;
+import com.faleicadev.workshopmongo.dto.UserDto;
 import com.faleicadev.workshopmongo.service.UserService;
 
 @RestController
@@ -19,10 +23,18 @@ public class UserResource {
 	private UserService serviceUser;
 	
 	
-	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<UserDto>> findAll(){
 		List<User> list = serviceUser.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDto> listDto = list.stream().map(x -> new UserDto(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+		
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<UserDto> findById(@PathVariable String id){
+		User obj = serviceUser.findById(id);
+		return ResponseEntity.ok().body(new UserDto(obj));
 		
 	}
 	
--- src/main/java/com/faleicadev/workshopmongo/service/UserService.java
+++ src/main/java/com/faleicadev/workshopmongo/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.faleicadev.workshopmongo.domain.User;
 import com.faleicadev.workshopmongo.repository.UserRepository;
+import com.faleicadev.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +17,16 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return userRepo.findAll();	
+	}
+	
+	public User findById(String id) {
+		User user = userRepo.findById(id).get();
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+			
+		}
+		return user;
+		
 	}
 
 }
